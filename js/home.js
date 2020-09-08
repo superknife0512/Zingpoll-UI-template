d45f04615cb3556c5b14d71c56b31256bdf77d81
ToanPhan <superknife0512@gmail.com>
2020-09-08 15:20:32 +0700

    finish hero area

---
+++ js/home.js
@@ -0,0 +1,38 @@
+new Vue({
+  el: '#home',
+  data: {
+    listOfStep: [
+      {
+        content: 'Enter your question',
+        color: '#56CCF2'
+      },
+      {
+        content: 'Choose poll type',
+        color: '#2D9CDB'
+      },
+      {
+        content: 'Change poll setting',
+        color: '#2F80ED'
+      },
+      {
+        content: 'Press CREATE!',
+        color: '#6B76D9'
+      },
+    ],
+    listOfLanguages: [
+      {
+        img: './images/Icons/VN.png',
+        desc: 'Viet nam'
+      },
+      {
+        img: './images/Icons/UK.jpg',
+        desc: 'English'
+      },
+      {
+        img: './images/Icons/Indo.jpg',
+        desc: 'Indonesia'
+      }
+    ],
+    showLangs: false
+  }
+})

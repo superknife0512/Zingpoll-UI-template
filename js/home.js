93eb099639a483a88b215b12d589f9c737c8894e
ToanPhan <superknife0512@gmail.com>
2020-09-09 15:56:54 +0700

    finish 2 popup

--- js/home.js
+++ js/home.js
@@ -35,7 +35,41 @@ new Vue({
       }
     ],
     showLangs: false,
-    questionType: 'Single'
+    questionType: 'Single',
+    listOfFeatures: [
+      {
+        img: './images/UI/simple.svg',
+        title: 'Simple',
+        desc: 'Create your poll with just 4 easy steps.'
+      },
+      {
+        img: './images/UI/accessible.svg',
+        title: 'Accessible',
+        desc: 'Access your poll anytime, anywhere, via most of devices with an Internet connection.'
+      },
+      {
+        img: './images/UI/illustrative.svg',
+        title: 'Illustrative',
+        desc: 'Visualize your poll in various types of charts.'
+      },
+      {
+        img: './images/UI/exportable.svg',
+        title: 'Exportable',
+        desc: 'Keep a record of your poll in PDF, Excel or printed paper.'
+      },
+      {
+        img: './images/UI/private.svg',
+        title: 'Private',
+        desc: 'Secure your poll with password if you wish to keep privacy.'
+      },
+      {
+        img: './images/UI/sharable.svg',
+        title: 'Sharable',
+        desc: 'Get more response by promoting your poll on Facebook, Google +, Twitter and emails.'
+      },
+    ],
+    signinPopup: false,
+    signupPopup: false,
   },
 
   methods: {
@@ -45,6 +79,10 @@ new Vue({
       } else {
         this.questionType = 'Single'
       }
+    },
+    switchAuth() {
+      this.signinPopup = !this.signinPopup;
+      this.signupPopup = !this.signupPopup
     }
   }
 })
---
+++ readme.md
@@ -0,0 +1,5 @@
+#### Zingpoll template
+
+1. Project using *scss* and *BEM* methodology to build style, you can install *watch sass extension* in VsCode to help you compile sass code without using webpack or npm.
+
+2. Project use **Vuejs** to handle javascript part in our app, instead of interacting with DOM directly: [https://vuejs.org/v2/guide/]
